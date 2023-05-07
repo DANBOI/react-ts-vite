9dfa03ee939d70bb7da3ec89f7388507da32ee01
chen <cycdanielboi@gmail.com>
2023-05-07 10:00:58 +0900

    use state,pass props and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import ListGroup from "./components/ListGroup";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Props = {};
 
+export default function App({}: Props) {
+  const items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+
+  const handleSelectItem = (item: string) => console.log(item);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <ListGroup items={items} heading="Game" onSelectItem={handleSelectItem} />
     </div>
-  )
+  );
 }
-
-export default App
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,32 @@
+import { MouseEvent, useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+export default function ListGroup({ items, heading, onSelectItem }: Props) {
+  // const handleClick = (event:MouseEvent)=>{console.log("123")}
+  const [SelectedIndex, setSelectedIndex] = useState(-1);
+  return (
+    <>
+      <h1>{heading} List</h1>
+      {items.length === 0 && <p>no items found!</p>}
+      <ul className="list-group">
+        {items.map((ele, i) => (
+          <li
+            key={i}
+            className={`list-group-item ${SelectedIndex === i ? "active" : ""}`}
+            onClick={() => {
+              setSelectedIndex(i);
+              onSelectItem(ele);
+            }}
+          >
+            {ele}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
